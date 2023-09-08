b6ac00a9af3e026c854ce63e7fcc0006740370eb
Minipomy <minipomy14@gmail.com>
2023-09-08 00:30:14 -0300

    Agregamos funciones

---
+++ Python - Programacion I/Ejercicios/funciones.py
@@ -0,0 +1,65 @@
+'''
+Crear una función que convierta grados Celsius a grados Fahrenheit. 
+Recibe un parámetro (grados Celsius) y 
+
+devuelve el resultado en grados Fahrenheit.
+'''
+
+def convertir_celsius_a_fahrenheit(grados_cenlsius:float) -> float:
+    grados_fahrenheit = (grados_cenlsius * 9/5) + 32
+    return(grados_fahrenheit)
+
+
+'''
+Crear una función que calcule el área de un círculo. 
+Recibe un parámetro (radio) y 
+
+devuelve el área del círculo.
+'''
+
+def area_circulo(radio:int) -> int:
+    area = float(3.14*radio**2)
+    return(area)
+
+
+'''
+Crear una función que calcule el descuento aplicado a un producto. 
+Recibe dos parámetros (precio original y precio con descuento) y 
+
+devuelve el porcentaje de descuento aplicado.
+'''
+
+def calcula_descuento_en_producto(precio_original, descuento):
+    descuento_aplicado = float(precio_original - (precio_original * (descuento/100)))
+    return(descuento_aplicado)
+
+
+'''
+Crear una función que calcule el promedio de edad de un grupo de personas. 
+Recibe una lista de edades y 
+devuelve el promedio.
+'''
+
+lista_alumnos = [22,33,55,67,11]
+
+def promedio_edades_personas(lista:list) -> int:
+    acumulador_edades = 0
+    contador_edades = 0
+    for edad in lista:
+        acumulador_edades += edad
+        contador_edades += 1
+    return(acumulador_edades / contador_edades)
+
+#---------------------------EJ1------------------------------
+print(f'{convertir_celsius_a_fahrenheit(35.3):.2f}')
+#---------------------------EJ2------------------------------
+print(f'{area_circulo(2):.2f}')
+#---------------------------EJ3------------------------------
+print(f'{calcula_descuento_en_producto(500, 10):.2f}')
+#---------------------------EJ4------------------------------
+print(promedio_edades_personas(lista_alumnos))
+#---------------------------EJ5------------------------------
+print()
+#---------------------------EJ2------------------------------
+print()
+
---
+++ Python - Programacion I/Ejercicios/stark_industries.py
@@ -0,0 +1,406 @@
+lista_personajes =\
+[
+  {
+    "nombre": "Howard the Duck",
+    "identidad": "Howard (Last name unrevealed)",
+    "empresa": "Marvel Comics",
+    "altura": "79.349999999999994",
+    "peso": "18.449999999999999",
+    "genero": "M",
+    "color_ojos": "Brown",
+    "color_pelo": "Yellow",
+    "fuerza": "2",
+    "inteligencia": ""
+  },
+  {
+    "nombre": "Rocket Raccoon",
+    "identidad": "Rocket Raccoon",
+    "empresa": "Marvel Comics",
+    "altura": "122.77",
+    "peso": "25.73",
+    "genero": "M",
+    "color_ojos": "Brown",
+    "color_pelo": "Brown",
+    "fuerza": "5",
+    "inteligencia": "average"
+  },
+  {
+    "nombre": "Wolverine",
+    "identidad": "Logan",
+    "empresa": "Marvel Comics",
+    "altura": "160.69999999999999",
+    "peso": "135.21000000000001",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "Black",
+    "fuerza": "35",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Black Widow",
+    "identidad": "Natasha Romanoff",
+    "empresa": "Marvel Comics",
+    "altura": "170.78999999999999",
+    "peso": "59.340000000000003",
+    "genero": "F",
+    "color_ojos": "Green",
+    "color_pelo": "Auburn",
+    "fuerza": "15",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Mystique",
+    "identidad": "Raven Darkholme",
+    "empresa": "Marvel Comics",
+    "altura": "178.65000000000001",
+    "peso": "54.960000000000001",
+    "genero": "F",
+    "color_ojos": "Yellow (without irises)",
+    "color_pelo": "Red / Orange",
+    "fuerza": "15",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Spider-Man",
+    "identidad": "Peter Parker",
+    "empresa": "Marvel Comics",
+    "altura": "178.28",
+    "peso": "74.25",
+    "genero": "M",
+    "color_ojos": "Hazel",
+    "color_pelo": "Brown",
+    "fuerza": "55",
+    "inteligencia": "high"
+  },
+  {
+    "nombre": "Storm",
+    "identidad": "Ororo Munroe",
+    "empresa": "Marvel Comics",
+    "altura": "180.72",
+    "peso": "57.5",
+    "genero": "F",
+    "color_ojos": "Blue",
+    "color_pelo": "White",
+    "fuerza": "10",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Gamora",
+    "identidad": "Gamora Zen Whoberi Ben Titan",
+    "empresa": "Marvel Comics",
+    "altura": "183.65000000000001",
+    "peso": "77.769999999999996",
+    "genero": "F",
+    "color_ojos": "Yellow",
+    "color_pelo": "Black",
+    "fuerza": "85",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Thing",
+    "identidad": "Ben Grimm",
+    "empresa": "Marvel Comics",
+    "altura": "183.55000000000001",
+    "peso": "225.41999999999999",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "No Hair",
+    "fuerza": "85",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Thor",
+    "identidad": "Thor Odinson",
+    "empresa": "Marvel Comics",
+    "altura": "198.34999999999999",
+    "peso": "288.61000000000001",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "Blond",
+    "fuerza": "100",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Colossus",
+    "identidad": "Piotr Nikolaievitch Rasputin",
+    "empresa": "Marvel Comics",
+    "altura": "226.13",
+    "peso": "225.52000000000001",
+    "genero": "M",
+    "color_ojos": "Silver",
+    "color_pelo": "Black",
+    "fuerza": "85",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Hulk",
+    "identidad": "Bruce Banner",
+    "empresa": "Marvel Comics",
+    "altura": "244.40000000000001",
+    "peso": "630.89999999999998",
+    "genero": "M",
+    "color_ojos": "Green",
+    "color_pelo": "Green",
+    "fuerza": "100",
+    "inteligencia": "high"
+  },
+  {
+    "nombre": "Groot",
+    "identidad": "Groot",
+    "empresa": "Marvel Comics",
+    "altura": "701.12",
+    "peso": "4.8200000000000003",
+    "genero": "M",
+    "color_ojos": "Yellow",
+    "color_pelo": "",
+    "fuerza": "85",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Daredevil",
+    "identidad": "Matt Murdock",
+    "empresa": "Marvel Comics",
+    "altura": "183.68000000000001",
+    "peso": "90.450000000000003",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "Red",
+    "fuerza": "15",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Nick Fury",
+    "identidad": "Nicholas Joseph Fury",
+    "empresa": "Marvel Comics",
+    "altura": "185.88",
+    "peso": "99.280000000000001",
+    "genero": "M",
+    "color_ojos": "Brown",
+    "color_pelo": "Brown / White",
+    "fuerza": "15",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Punisher",
+    "identidad": "Frank Castle",
+    "empresa": "Marvel Comics",
+    "altura": "183.25999999999999",
+    "peso": "90.900000000000006",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "Black",
+    "fuerza": "20",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Star-Lord",
+    "identidad": "Peter Jason Quill",
+    "empresa": "Marvel Comics",
+    "altura": "188.25",
+    "peso": "79.209999999999994",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "Blond",
+    "fuerza": "20",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Deadpool",
+    "identidad": "Wade Wilson",
+    "empresa": "Marvel Comics",
+    "altura": "188.0",
+    "peso": "95.319999999999993",
+    "genero": "M",
+    "color_ojos": "Brown",
+    "color_pelo": "No Hair",
+    "fuerza": "35",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Captain America",
+    "identidad": "Steve Rogers",
+    "empresa": "Marvel Comics",
+    "altura": "188.00999999999999",
+    "peso": "108.94",
+    "genero": "M",
+    "color_ojos": "blue",
+    "color_pelo": "blond",
+    "fuerza": "20",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Ghost Rider",
+    "identidad": "Johnny Blaze",
+    "empresa": "Marvel Comics",
+    "altura": "188.50999999999999",
+    "peso": "99.200000000000003",
+    "genero": "M",
+    "color_ojos": "Red",
+    "color_pelo": "No Hair",
+    "fuerza": "55",
+    "inteligencia": "average"
+  },
+  {
+    "nombre": "Blade",
+    "identidad": "Eric Brooks",
+    "empresa": "Marvel Comics",
+    "altura": "188.44999999999999",
+    "peso": "97.400000000000006",
+    "genero": "M",
+    "color_ojos": "Brown",
+    "color_pelo": "Black",
+    "fuerza": "30",
+    "inteligencia": "good"
+  },
+  {
+    "nombre": "Hawkeye",
+    "identidad": "Clint Barton",
+    "empresa": "Marvel Comics",
+    "altura": "191.00999999999999",
+    "peso": "104.93000000000001",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "Blond",
+    "fuerza": "15",
+    "inteligencia": "average"
+  },
+  {
+    "nombre": "Drax the Destroyer",
+    "identidad": "Arthur Sampson Douglas",
+    "empresa": "Marvel Comics",
+    "altura": "193.00999999999999",
+    "peso": "306.42000000000002",
+    "genero": "M",
+    "color_ojos": "Red",
+    "color_pelo": "No Hair",
+    "fuerza": "80",
+    "inteligencia": "average"
+  },
+  {
+    "nombre": "Iron Man",
+    "identidad": "Tony Stark",
+    "empresa": "Marvel Comics",
+    "altura": "198.91",
+    "peso": "191.88",
+    "genero": "M",
+    "color_ojos": "Blue",
+    "color_pelo": "Black",
+    "fuerza": "85",
+    "inteligencia": "high"
+  }
+]
+
+'''
+Analizar detenidamente el set de datos
+Recorrer la lista imprimiendo por consola el nombre de cada superhéroe
+Recorrer la lista imprimiendo por consola nombre de cada superhéroe junto a la altura del mismo
+Recorrer la lista y determinar cuál es el superhéroe más alto (MÁXIMO)
+Recorrer la lista y determinar cuál es el superhéroe más bajo (MÍNIMO)
+Recorrer la lista y determinar la altura promedio de los  superhéroes (PROMEDIO)
+Informar cual es la identidad del superhéroe asociado a cada uno de los indicadores anteriores (MÁXIMO, MÍNIMO)
+Calcular e informar cual es el superhéroe más y menos pesado.
+Ordenar el código implementando una función para cada uno de los valores informados.
+Construir un menú que permita elegir qué dato obtener
+'''
+
+
+def imprimir_nombres_de_superheroes(lista:list) -> list:
+    for heroe in lista:
+        nombre = heroe.get('nombre', "No se encontro")
+        print(nombre)
+
+def imprimir_altura_de_superheroes(lista:list) -> list:
+    for heroe in lista:
+        nombre = heroe.get('nombre', "No se encontro")
+        altura = heroe.get('altura', "No se encontro")
+        print(nombre, altura)
+
+def heroe_mas_alto(lista:list) -> str:
+    flag = True
+    heroe_mas_alto = ""
+    altura_mayor = ""
+    for heroe in lista:
+        if(flag == True):
+            heroe_mas_alto = heroe.get('nombre', "No se encontro")
+            altura_mayor = heroe.get('altura', "No se encontro")
+            flag = False
+        elif(altura_mayor >= heroe.get('altura', "No se encontro")):
+            heroe_mas_alto = heroe.get('nombre', "No se encontro")
+            altura_mayor = heroe.get('altura', "No se encontro")
+    print("El heroe mas alto es: ",heroe_mas_alto, altura_mayor)
+    return(heroe_mas_alto)
+
+def heroe_mas_bajo(lista:list) -> str:
+    flag = True
+    heroe_mas_bajo = ""
+    altura_menor = ""
+    for heroe in lista:
+        if(flag == True):
+            heroe_mas_bajo = heroe.get('nombre', "No se encontro")
+            altura_menor = heroe.get('altura', "No se encontro")
+            flag = False
+        elif(altura_menor <= heroe.get('altura', "No se encontro")):
+            heroe_mas_bajo = heroe.get('nombre', "No se encontro")
+            altura_menor = heroe.get('altura', "No se encontro")
+    print("El heroe mas bajo es: ",heroe_mas_bajo, altura_menor)
+    return(heroe_mas_bajo)
+
+def promedio_altura_de_heroes(lista:list) -> str:
+    acumulador_altura = 0
+    contador_altura = 0
+    for heroe in lista:
+        acumulador_altura += float(heroe.get('altura', 0))
+        contador_altura += 1
+    promedio = acumulador_altura / contador_altura
+    print("El promedio de la altura de los heroes es: ",f'{promedio:.2f}')
+    return(promedio)
+
+def identidades_maximos_minimos(lista:list) -> str:
+    for heroe in lista:
+        if(heroe.get('nombre') == heroe_mas_bajo(lista)):
+                identidad_menor = heroe.get('identidad', 'No se encontro')
+        elif(heroe.get('nombre') == heroe_mas_alto(lista)):
+                identidad_mayor = heroe.get('identidad', 'No se encontro')
+    print("identidadMA ", identidad_mayor, "identidadME " ,identidad_menor)
+
+
+imprimir_nombres_de_superheroes(lista_personajes)
+imprimir_altura_de_superheroes(lista_personajes)
+heroe_mas_alto(lista_personajes)
+heroe_mas_bajo(lista_personajes)
+promedio_altura_de_heroes(lista_personajes)
+identidades_maximos_minimos(lista_personajes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+persona = 'Maxi'
+
+
+
+def imprimir_hola_mundo():
+    return(print("Hola mundo"))
+
+def imprimir_hola_mundo_persona(persona:str) -> str:
+    return(print(f"Hola mundo {persona}"))
+
+
+
+imprimir_hola_mundo()
+imprimir_hola_mundo()
+
+
+imprimir_hola_mundo_persona('Juan')
+
